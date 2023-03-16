7c92a6d84cc2fc1480ec4918b8bff2dc0f68aeb3
Ehteshan Alam <122683387+ehteshan62@users.noreply.github.com>
2023-03-16 20:56:29 +0530

    Add files via upload

---
+++ chatGPT.py
@@ -0,0 +1,38 @@
+import openai
+import speech_recognition
+import pyttsx3
+
+
+ #  Set up the OpenAI API client
+openai.api_key = "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"
+
+# this loop will let us ask questions continuously and behave like ChatGPT
+while True:
+    # Set up the model and prompt
+    model_engine = "text-davinci-003"
+
+    prompt = input('Enter new prompt: ')
+
+    if 'exit' in prompt or 'quit' in prompt:
+        break
+
+    # Generate a response
+    completion = openai.Completion.create(engine=model_engine,prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.5,)
+
+    # extracting useful part of response
+    response = completion.choices[0].text
+
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+
+    engine.setProperty('voice', voices[1].id)
+
+
+    def speak(audio):
+        engine.say(audio)
+        print(audio)
+        engine.runAndWait()
+
+
+     #  printing response
+    speak(response)
